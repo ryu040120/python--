2c2a742ac436e279b21b71d89ad636276f925e2f
ryu040120 <ryuzougo@gmail.com>
2025-07-17 04:24:32 +0900

    1

---
+++ example.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+def greet():
+    return("こんにちは")
+
+print(greet())
+
+def print_name(name):
+    return(f"私の名前は{name}です") 
+
+print(print_name("ryu"))
+
+def get_greet():
+    return "おはようございます"
+
+print(get_greet())
+
+def add(a,b):
+    return a + b
+
+print(add(2,3))
